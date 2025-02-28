fae6a891a38ea14dfa5c010574b59a85d832ec96
hunglq <hunglq.dev@gmail.com>
2025-02-28 20:30:48 +0700

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
+
   experimental: {
     sharedPrerenderData: true
   },
+
   ssr: false,
+
   css: [
     'vue-virtual-scroller/dist/vue-virtual-scroller.css',
     '@/assets/css/main.css',
@@ -15,35 +18,43 @@ export default defineNuxtConfig({
     '@/assets/scss/themes-ant-vue.scss',
     '@/assets/scss/themes-ag-grid-vue.scss'
   ],
+
   build: {
     transpile: ['']
   },
+
   nitro: {
     preset: 'node-server',
     compressPublicAssets: true
   },
+
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     }
   },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
   },
+
   components: true,
+
   plugins: [
     { src: '~/plugins/axios', mode: 'client' },
     { src: '~/plugins/vue-image-crop-upload', mode: 'client' },
     { src: '~/plugins/vue-virtual-scroller', mode: 'client' },
     { src: '~/plugins/first', mode: 'client' },
   ],
+
   runtimeConfig: {
     public: {
     }
   },
+
   modules: [
     '@nuxtjs/device',
     'nuxt-gtag',
@@ -54,6 +65,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxtjs/i18n'
   ],
+
   gtm: {
     id: 'GTM-NVZX6GQP',
     defer: false,
@@ -65,13 +77,15 @@ export default defineNuxtConfig({
     trackOnNextTick: false,
     devtools: true
   },
+
   gtag: {
     id: 'G-FK4ZY1C381',
     config: {
       page_title: 'Phần mềm phân tích chứng khoán số 1 Việt Nam'
     }
   },
+
   i18n: {
     vueI18n: './i18n.config.ts',
-  }
+  },
 });
