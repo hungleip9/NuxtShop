f2683c4e3eb78ac25858c28192ed5c608f867564
hungleip9@gmail.com <hungleip9@gmail.com>
2025-03-10 11:17:06 +0700

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-
+  ssr: false,
   experimental: {
     sharedPrerenderData: true
   },
-
-  ssr: false,
-
   css: [
     'vue-virtual-scroller/dist/vue-virtual-scroller.css',
     '@/assets/css/main.css',
@@ -18,74 +16,68 @@ export default defineNuxtConfig({
     '@/assets/scss/themes-ant-vue.scss',
     '@/assets/scss/themes-ag-grid-vue.scss'
   ],
-
   build: {
     transpile: ['']
   },
-
   nitro: {
     preset: 'node-server',
     compressPublicAssets: true
   },
-
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     }
   },
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
   },
-
   components: true,
-
   plugins: [
     { src: '~/plugins/axios', mode: 'client' },
     { src: '~/plugins/vue-image-crop-upload', mode: 'client' },
     { src: '~/plugins/vue-virtual-scroller', mode: 'client' },
     { src: '~/plugins/first', mode: 'client' },
   ],
-
   runtimeConfig: {
     public: {
+      baseUrlNews: '',
+      baseUrlAuth: '',
+      baseUrlEconomic: '',
+      baseUrlStockexchange: '',
+      baseUrlCommunity: '',
+      fileS3url: '',
+      connectWebsocketurl: '',
+      logo: '',
+      version: '',
+      qos: '',
+      timeReconnectMqtt: 0,
+      timeShowPopup: 0,
+      topic: [],
+      gtm: {
+        id: 'GTM-NVZX6GQP',
+        queryParams: {
+          gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr'
+        },
+        defer: false,
+        compatibility: false,
+        enabled: true,
+        debug: false,
+        loadScript: true,
+        enableRouterSync: true,
+        trackOnNextTick: false,
+        devtools: true
+      }
     }
   },
 
   modules: [
     '@nuxtjs/device',
-    'nuxt-gtag',
-    '@zadigetvoltaire/nuxt-gtm',
     '@ant-design-vue/nuxt',
     'nuxt-build-cache',
     '@vueuse/nuxt',
     '@nuxt/image',
-    '@nuxtjs/i18n'
-  ],
-
-  gtm: {
-    id: 'GTM-NVZX6GQP',
-    defer: false,
-    compatibility: false,
-    enabled: true,
-    debug: false,
-    loadScript: true,
-    enableRouterSync: true,
-    trackOnNextTick: false,
-    devtools: true
-  },
-
-  gtag: {
-    id: 'G-FK4ZY1C381',
-    config: {
-      page_title: 'Phần mềm phân tích chứng khoán số 1 Việt Nam'
-    }
-  },
-
-  i18n: {
-    vueI18n: './i18n.config.ts',
-  },
+  ]
 });
